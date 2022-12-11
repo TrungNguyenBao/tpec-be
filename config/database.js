2169153db6795523fe8bd48ec13906a421640d75
TuyenDv <vantuyenict@gmail.com>
2022-12-11 14:12:08 +0700

    .

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
     client: env("DATABASE_CLIENT", "mysql"),
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3312),
-      database: env("DATABASE_NAME", "tpec"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "funzy_game"),
       user: env("DATABASE_USERNAME", "tpec_admin"),
       password: env("DATABASE_PASSWORD", "acb@123"),
       ssl: env.bool("DATABASE_SSL", false),
